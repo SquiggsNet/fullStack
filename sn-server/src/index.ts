99b92d8df370c8574edd1c34a90049956e389d57
squiggsnet <squiggs.rafael@gmail.com>
2021-05-22 11:50:55 -0300

    chore: add typeorm and initialize create connection

--- sn-server/src/index.ts
+++ sn-server/src/index.ts
@@ -1,14 +1,26 @@
 import express from 'express';
+import { createConnection } from 'typeorm';
 
 const main = async () => {
+    await createConnection({
+    type: 'postgres',
+    database: 'sqn-db',
+    username: 'postgres',
+    password: 'postgres',
+    logging: true,
+    synchronize: true,
+    entities: []
+  })
+
   const app = express();
   app.get('/', (_, res) => {
     res.send("hello");
   });
+
   const port = 4000;
   app.listen(port, () => {
     console.log(`sn-server started: listening on ${port}`);
   })
 }
 
-main()
+main();
