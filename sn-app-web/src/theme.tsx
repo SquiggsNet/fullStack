69abde643cac6d645df6fe2b4a7874840caf7296
squiggsnet <squiggs.rafael@gmail.com>
2021-06-02 23:43:48 -0300

    wip: toggle dark and light themes

--- sn-app-web/src/theme.tsx
+++ sn-app-web/src/theme.tsx
@@ -1,5 +1,5 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
-import { createBreakpoints, mode } from '@chakra-ui/theme-tools'
+import { createBreakpoints } from '@chakra-ui/theme-tools'
 
 const fonts = {
   color: "#111331",
